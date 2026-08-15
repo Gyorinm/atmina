ce366faa72812331fd2de2f38d08bc78e16cdf7f
Gyorinm <sarawahida111@gmail.com>
2026-08-15 13:13:40 +0100

    feat: handle shared order deep links

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -1,32 +1,38 @@
+import 'dart:async';
+import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-
 import '../core/theme/app_theme.dart';
+import '../features/orders/domain/order_payload.dart';
+import '../features/orders/presentation/order_link_screen.dart';
 import '../features/products/presentation/screens/home_screen.dart';
 
-class AtminaApp extends StatelessWidget {
+class AtminaApp extends StatefulWidget {
   const AtminaApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Atmina POS',
-      theme: AppTheme.lightTheme,
-      locale: const Locale('ar'),
-      supportedLocales: const [
-        Locale('ar'),
-        Locale('en'),
-      ],
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      home: const Directionality(
-        textDirection: TextDirection.rtl,
-        child: HomeScreen(),
-      ),
-    );
+  @override State<AtminaApp> createState() => _AtminaAppState();
+}
+class _AtminaAppState extends State<AtminaApp> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+  StreamSubscription<Uri>? _linkSubscription;
+  @override void initState() { super.initState(); _initDeepLinks(); }
+  @override void dispose() { _linkSubscription?.cancel(); super.dispose(); }
+  Future<void> _initDeepLinks() async {
+    final links = AppLinks();
+    final initial = await links.getInitialLink();
+    if (initial != null) _openLink(initial);
+    _linkSubscription = links.uriLinkStream.listen(_openLink);
   }
+  void _openLink(Uri uri) {
+    if (uri.scheme != 'atmina' || uri.host != 'order' || uri.pathSegments.isEmpty) return;
+    try {
+      final payload = OrderPayload.decode(uri.pathSegments.first);
+      _navigatorKey.currentState?.push(MaterialPageRoute(builder: (_) => OrderLinkScreen(payload: payload)));
+    } catch (_) {}
+  }
+  @override Widget build(BuildContext context) => MaterialApp(
+    navigatorKey: _navigatorKey, debugShowCheckedModeBanner: false, title: 'Atmina POS', theme: AppTheme.lightTheme,
+    locale: const Locale('ar'), supportedLocales: const [Locale('ar'), Locale('en')],
+    localizationsDelegates: const [GlobalMaterialLocalizations.delegate, GlobalWidgetsLocalizations.delegate, GlobalCupertinoLocalizations.delegate],
+    home: const Directionality(textDirection: TextDirection.rtl, child: HomeScreen()),
+  );
 }
