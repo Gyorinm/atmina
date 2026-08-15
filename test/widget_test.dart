a2f7ae932668894d08983c317962f7318ccd4f3d
Gyorinm <sarawahida111@gmail.com>
2026-08-15 19:51:24 +0100

    test: mock persisted merchant role so widget test skips role selection screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,12 @@
 import 'package:atmina_pos/src/app/app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   testWidgets('يعرض الواجهة الرئيسية للتطبيق', (tester) async {
+    SharedPreferences.setMockInitialValues({'atmina_app_role': 'merchant'});
+
     await tester.pumpWidget(
       const ProviderScope(
         child: AtminaApp(),
