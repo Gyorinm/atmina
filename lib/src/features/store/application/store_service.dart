cfb8e85eecdc83dc3cf0071c12347e94f2d29ca1
Gyorinm <sarawahida111@gmail.com>
2026-08-15 20:12:09 +0100

    feat: add StoreService (code generation, payload building)

---
+++ lib/src/features/store/application/store_service.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+
+import '../../products/domain/models/product.dart';
+import '../domain/store_payload.dart';
+
+class StoreService {
+  static final Random _random = Random.secure();
+
+  static const String _codeAlphabet = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ';
+
+  static String generateStoreCode() {
+    final buffer = StringBuffer('ATM-');
+    var checksum = 0;
+    for (var i = 0; i < 8; i++) {
+      final index = _random.nextInt(_codeAlphabet.length);
+      checksum = (checksum + index * (i + 1)) % _codeAlphabet.length;
+      buffer.write(_codeAlphabet[index]);
+    }
+    buffer.write(_codeAlphabet[checksum]);
+    return buffer.toString();
+  }
+
+  StorePayload buildPayload({
+    required String storeCode,
+    required String storeName,
+    required String whatsappNumber,
+    required List<Product> products,
+  }) {
+    return StorePayload(
+      storeCode: storeCode,
+      storeName: storeName,
+      whatsappNumber: whatsappNumber,
+      generatedAt: DateTime.now().toIso8601String(),
+      items: products
+          .map((p) => StorePayloadItem(
+                internalCode: p.barcode,
+                name: p.name,
+                category: p.category,
+                price: p.price,
+                stockQuantity: p.stockQuantity,
+              ))
+          .toList(growable: false),
+    );
+  }
+}
