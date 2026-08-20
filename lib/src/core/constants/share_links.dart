dface5a7460c030b19be3f068a60f980fec46c66
Gyorinm <sarawahida111@gmail.com>
2026-08-20 01:24:57 +0100

    feat: تقصير رابط الطلبية بنشر بياناتها على الخادم بدل تضمينها في الرابط

--- lib/src/core/constants/share_links.dart
+++ lib/src/core/constants/share_links.dart
@@ -6,7 +6,10 @@
 abstract final class ShareLinks {
   static const String _baseUrl = 'https://atmina-store-api.o2730884.workers.dev/l';
 
-  static String storeLink(String encodedPayload) => '$_baseUrl#store/$encodedPayload';
+  static String storeLink(String storeCode) => '$_baseUrl#store/$storeCode';
 
-  static String orderLink(String encodedPayload) => '$_baseUrl#order/$encodedPayload';
+  /// رابط الطلبية يحمل رمزها القصير فقط (مثلاً 1755701234567-482)، بعد
+  /// أن أصبحت بيانات الطلبية الكاملة تُنشر على الخادم أولاً بدل تضمينها
+  /// مضغوطة داخل الرابط نفسه، مما يقصّره بشكل كبير.
+  static String orderLink(String orderCode) => '$_baseUrl#order/$orderCode';
 }
